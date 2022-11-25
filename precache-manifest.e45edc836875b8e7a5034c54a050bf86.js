04fa2e67797235b3d1e8d0c36279f0bfbc8ac62a
Kanak-debug <114392790+Kanak-debug@users.noreply.github.com>
2022-11-25 13:41:22 +0530

    first commit

---
+++ precache-manifest.e45edc836875b8e7a5034c54a050bf86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10b68a8b0b674cfeda67",
+    "url": "/write-you/static/css/main.2d97fb83.chunk.css"
+  },
+  {
+    "revision": "10b68a8b0b674cfeda67",
+    "url": "/write-you/static/js/main.3e5ab175.chunk.js"
+  },
+  {
+    "revision": "3f919ceb272fdcb87887",
+    "url": "/write-you/static/js/runtime~main.bb8c7170.js"
+  },
+  {
+    "revision": "9162cf41c7fa6eafdcbb",
+    "url": "/write-you/static/js/2.518c7694.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/write-you/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "fb6a8a50757de804bbf2abcb14c7a737",
+    "url": "/write-you/index.html"
+  }
+];
